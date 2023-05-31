d492eb01ccf44a47bc59516c0f223016a63fa0ce
Bobby Donchev <contact@donchev.is>
2023-05-31 08:59:22 +0200

    add stop sequeuence for completion

--- financy/open_ai/jsonl.py
+++ financy/open_ai/jsonl.py
@@ -4,7 +4,7 @@ Entry = namedtuple("Entry", ["prompt", "completion"])
 
 # Each prompt should end with a fixed separator to inform the model when the prompt ends and the completion begins. 
 seperator = "\n\nDecision:"
-
+stop_sequence = "\n"
 
 def write_entry(entry: Entry, file_name: str = "financy_form.jsonl"):
     with open(file_name, "a") as f:
@@ -12,4 +12,5 @@ def write_entry(entry: Entry, file_name: str = "financy_form.jsonl"):
 
         # we encode to perserve the newline character
         encodedPrompt = f"{entry.prompt}{seperator}".encode('unicode_escape').decode('utf-8')
-        f.write(f'{{"prompt": "{encodedPrompt}", "completion": "{entry.completion}"}}\n')
+        encodedCompletion = f"{entry.completion}{stop_sequence}".encode('unicode_escape').decode('utf-8')
+        f.write(f'{{"prompt": "{encodedPrompt}", "completion": "{encodedCompletion}"}}\n')
